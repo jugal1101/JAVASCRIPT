b5e10421e479c0626952abc506c9c282557753bf
jugal1101 <8733jugalrana@gmail.com>
2025-04-25 14:53:50 +0530

    Add files via upload

---
+++ Function/main.js
@@ -0,0 +1,49 @@
+// 1. wap to print a 1 to 5 table using a function
+
+// function printTables() {
+//     for (let i = 1; i <= 10; i++) {
+//      for (let j = 1; j <= 5; j++) {
+//             document.writeln(j + " x " + i + " = " + (j * i) + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
+//         }
+//         document.writeln("<br>");
+//     }
+// }
+
+// printTables();
+
+
+
+
+
+
+// 2. wap to print a 1 to n table using a function.
+// let number = parseInt(prompt("Enter a number"));
+// function tables(n){
+//     for(let i=1; i<=number; i++){
+//         for(let j=1; j<=10; j++){
+//        document.writeln( i + "*" + j + "=" + (i*j)+ "<br>")
+//     }
+//     document.writeln("<br>")
+// }
+// }
+// tables();
+
+
+
+
+
+
+
+
+// 3. wap to print a 1 to n table using a function.
+// let a = parseInt(prompt("Enter number"))
+// function table(a) {
+//     for (let i = 1; i <= a; i++) {
+//       for (let j = 1; j <= 10; j++) {
+//         document.writeln(j + "*" + i + "=" + j * i)
+//              document.writeln("<br>")
+//       }
+//       document.writeln("<br>")
+//     }
+//   }
+//   table();
